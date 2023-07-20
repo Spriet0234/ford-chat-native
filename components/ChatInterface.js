077f2b2ac128fd31e2ceafccd47950ec9d1d9152
Santiago Prieto <sanpriet@publicisgroupe.net>
2023-07-20 16:07:22 -0400

    a

--- components/ChatInterface.js
+++ components/ChatInterface.js
@@ -30,6 +30,10 @@ const ChatInterface = () => {
   const [selected, setSelected] = useState("");
   const [name, setName] = useState("");
   const [menuVisible, setMenuVisible] = useState(false);
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const [open4, setOpen4] = useState(false);
 
   //MAP CODE--------------------------
   function calculateDistance(lat1, lon1, lat2, lon2) {
@@ -448,121 +452,306 @@ const ChatInterface = () => {
             </View>
             <View>
               <TouchableOpacity
+                onPress={() => {
+                  if (open1) {
+                    setOpen1(false);
+                  } else {
+                    setOpen1(true);
+                  }
+                }}
                 style={{
                   backgroundColor: "#00095B",
                   borderRadius: 5,
                   padding: 10,
-                  marginBottom: 10,
+                  marginBottom: 0,
                 }}
               >
                 <Text style={{ color: "white", fontSize: 16, fontWeight: 500 }}>
                   Buying a Ford
                 </Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "white",
-                  color: "#00095B",
-                  borderRadius: 5,
-                  padding: 10,
-                  marginBottom: 0,
-                  borderColor: "#00095B",
-                  borderWidth: 1,
-                  marginTop: -10,
-                }}
-              >
-                <Text
-                  style={{ color: "#00095B", fontSize: 16, fontWeight: 500 }}
-                >
-                  Info on a specific Ford
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "white",
-                  color: "#00095B",
-                  borderRadius: 5,
-                  padding: 10,
-                  marginBottom: 0,
-                  borderColor: "#00095B",
-                  borderWidth: 1,
-                }}
-              >
-                <Text
-                  style={{ color: "#00095B", fontSize: 16, fontWeight: 500 }}
-                >
-                  Car recommendation
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "white",
-                  color: "#00095B",
-                  borderRadius: 5,
-                  padding: 10,
-                  marginBottom: 0,
-                  borderColor: "#00095B",
-                  borderWidth: 1,
-                }}
-              >
-                <Text
-                  style={{ color: "#00095B", fontSize: 16, fontWeight: 500 }}
-                >
-                  Car pricing estimator{" "}
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "white",
-                  color: "#00095B",
-                  borderRadius: 5,
-                  padding: 10,
-                  marginBottom: 10,
-                  borderColor: "#00095B",
-                  borderWidth: 1,
-                }}
-              >
-                <Text
-                  style={{ color: "#00095B", fontSize: 16, fontWeight: 500 }}
-                >
-                  Find a dealership{" "}
-                </Text>
-              </TouchableOpacity>
+              {open1 && (
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                      marginTop: 0,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Info on a specific Ford
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Car recommendation
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Car pricing estimator{" "}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Find a dealership{" "}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Schedule a test drive{" "}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
               <TouchableOpacity
                 style={{
                   backgroundColor: "#00095B",
                   borderRadius: 5,
                   padding: 10,
-                  marginBottom: 10,
+                  marginTop: 10,
+                }}
+                onPress={() => {
+                  if (open2) {
+                    setOpen2(false);
+                  } else {
+                    setOpen2(true);
+                  }
                 }}
               >
                 <Text style={{ color: "white", fontSize: 16, fontWeight: 500 }}>
                   Existing Owner
                 </Text>
               </TouchableOpacity>
+              {open2 && (
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Maintenance request{" "}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Car resale{" "}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
               <TouchableOpacity
                 style={{
                   backgroundColor: "#00095B",
                   borderRadius: 5,
                   padding: 10,
-                  marginBottom: 10,
+                  marginTop: 10,
+                }}
+                onPress={() => {
+                  if (open3) {
+                    setOpen3(false);
+                  } else {
+                    setOpen3(true);
+                  }
                 }}
               >
                 <Text style={{ color: "white", fontSize: 16, fontWeight: 500 }}>
                   Info About Ford
                 </Text>
               </TouchableOpacity>
+              {open3 && (
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      marginBottom: 0,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Innovation and sustainability{" "}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
               <TouchableOpacity
                 style={{
                   backgroundColor: "#00095B",
                   borderRadius: 5,
                   padding: 10,
+                  marginTop: 10,
+                }}
+                onPress={() => {
+                  if (open4) {
+                    setOpen4(false);
+                  } else {
+                    setOpen4(true);
+                  }
                 }}
               >
                 <Text style={{ color: "white", fontSize: 16, fontWeight: 500 }}>
                   Know my car's price
                 </Text>
               </TouchableOpacity>
+              {open4 && (
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Electric vehicles{" "}
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "white",
+                      color: "#00095B",
+                      borderRadius: 5,
+                      padding: 10,
+                      borderColor: "#00095B",
+                      borderWidth: 1,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#00095B",
+                        fontSize: 16,
+                        fontWeight: 500,
+                      }}
+                    >
+                      Dealer vehicles{" "}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
             </View>
           </View>
         </View>
