bcf4aff9931ac4c3bb4d2ffe04b2de3d8acc8b6a
Santiago Prieto <sanpriet@publicisgroupe.net>
2023-07-27 14:21:11 -0400

    a

--- components/CarInfoTable.js
+++ components/CarInfoTable.js
@@ -1,53 +1,84 @@
-import { View } from 'react-native';
+import { View } from "react-native";
 
 import TableModel from "./TableModel";
 import Popup from "./Popup";
-import { useState } from 'react';
-import { Button, Text } from 'react-native-paper';
+import { useState } from "react";
+import { Button, Text } from "react-native-paper";
 
-import images from '../src/images/image_link.json'
-import { Image } from 'react-native';
-const tables={}
-const tableHead=["Make", "Model", "MSRP"]
-let onPresses=[]
+import images from "../src/images/image_link.json";
+import { Image } from "react-native";
+const tables = {};
+const tableHead = ["Make", "Model", "MSRP"];
+let onPresses = [];
 
-import '../src/styles/Table.css'
+//import '../src/styles/Table.css'
 
-export default function CarInfoTable({data}){
-    //Array which will be used to generate table
-  const [tableData, setTableData] = useState(data.map(car=>[car.model, car.trim, car.msrp]))
-  const [visible, setVisible] = useState(false)
-  const [popupTitle, setPopupTitle] = useState("Info about...")
-  const [popupContent, setPopupContent] = useState(<View>
-    <h1>Hello</h1>
-  </View>)
+export default function CarInfoTable({ data }) {
+  //Array which will be used to generate table
+  const [tableData, setTableData] = useState(
+    data.map((car) => [car.model, car.trim, car.msrp])
+  );
+  const [visible, setVisible] = useState(false);
+  const [popupTitle, setPopupTitle] = useState("Info about...");
+  const [popupContent, setPopupContent] = useState(
+    <View>
+      <h1>Hello</h1>
+    </View>
+  );
 
-  const showDialog=(dt)=>{
-    setPopupTitle("Info about this Ford "+dt.model)
-    setPopupContent(<View>
-        <Image source={`${images[dt.model][dt.trim]}`} style={{ alignSelf: "center", width: '300px', height: '200px' }}></Image>
-        <p>Trim: {dt.trim} <br/>
-        MSRP: {dt.msrp} <br/>
-        Body Size: {dt.body_size} <br/>
-        Body Style: {dt.body_style} <br/>
-        Seating Capacity: {dt.seating_capacity} <br/>
-        Drivetrain {dt.drivetrain} <br/>
-        Transmission: {dt.transmission} <br/>
-        Horsepower: {dt.horsepower} <br/>
+  const showDialog = (dt) => {
+    setPopupTitle("Info about this Ford " + dt.model);
+    setPopupContent(
+      <View>
+        <Image
+          source={`${images[dt.model][dt.trim]}`}
+          style={{ alignSelf: "center", width: "300px", height: "200px" }}
+        ></Image>
+        <p>
+          Trim: {dt.trim} <br />
+          MSRP: {dt.msrp} <br />
+          Body Size: {dt.body_size} <br />
+          Body Style: {dt.body_style} <br />
+          Seating Capacity: {dt.seating_capacity} <br />
+          Drivetrain {dt.drivetrain} <br />
+          Transmission: {dt.transmission} <br />
+          Horsepower: {dt.horsepower} <br />
         </p>
-    </View>)
-    setVisible(true)
-   }
+      </View>
+    );
+    setVisible(true);
+  };
 
-  const hideDialog=()=>{
-    setVisible(false)
-    }
+  const hideDialog = () => {
+    setVisible(false);
+  };
 
   return (
     <View>
-        <Popup title={popupTitle} visible={visible} content={popupContent} dismiss={hideDialog} actions={(<Button onPress={()=>{hideDialog()}}>Done</Button>)}/>
-        <Text style={{textAlign: 'center', fontSize: 15, margin: '2%'}}>Tap on a specific row for more information about that model.</Text>
-        <TableModel header={tableHead} table={tableData} onPresses={[showDialog, showDialog, showDialog]} params={data}/>
+      <Popup
+        title={popupTitle}
+        visible={visible}
+        content={popupContent}
+        dismiss={hideDialog}
+        actions={
+          <Button
+            onPress={() => {
+              hideDialog();
+            }}
+          >
+            Done
+          </Button>
+        }
+      />
+      <Text style={{ textAlign: "center", fontSize: 15, margin: "2%" }}>
+        Tap on a specific row for more information about that model.
+      </Text>
+      <TableModel
+        header={tableHead}
+        table={tableData}
+        onPresses={[showDialog, showDialog, showDialog]}
+        params={data}
+      />
     </View>
-  )
+  );
 }
